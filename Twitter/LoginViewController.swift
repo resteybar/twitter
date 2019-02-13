81decf426bdc7affaf8a952dc867a4a6f2f984d2
resteybar <resteybar@csumb.edu>
2019-02-12 17:01:09 -0800

    Created Login Button Function

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Raymond Esteybar on 2/12/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func on_login_button(_ sender: Any) {
+        print("Hello world!")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
